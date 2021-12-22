459ccffccc8abcb694ea1c8874bd86bb3d959906
LoatmanP <48681293+LoatmanP@users.noreply.github.com>
2021-12-22 15:38:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -100,6 +100,13 @@ fig.add_annotation(ax=0, ay=-150,
            arrowwidth=1
 
                    )
+fig.add_annotation(ax=0, ay=-75,
+                   x = pd.Timestamp('2016-12-01') + (pd.Timestamp('2017-01-31') - pd.Timestamp('2016-12-01'))/2, y = 1.4,
+            text="UX/Data Analyst Intern",
+            arrowhead=6,
+           arrowsize=2,
+           arrowwidth=1
+)
 
 
 
